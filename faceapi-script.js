143ffef7610553c50bdea4b8bb83f45feb4413d7
Senthil Kumaran <132420106+Senthilsk10@users.noreply.github.com>
2024-04-28 17:21:05 +0530

    Update faceapi-script.js

--- faceapi-script.js
+++ faceapi-script.js
@@ -1,9 +1,9 @@
 const imageUpload = document.getElementById('imageUpload')
 const message = document.getElementById('message');
 Promise.all([
-  faceapi.nets.faceRecognitionNet.loadFromUri('https://raw.githubusercontent.com/Senthilsk10/face-api-web/main/models/'),
-  faceapi.nets.faceLandmark68Net.loadFromUri('https://raw.githubusercontent.com/Senthilsk10/face-api-web/main/models/'),
-  faceapi.nets.ssdMobilenetv1.loadFromUri('https://raw.githubusercontent.com/Senthilsk10/face-api-web/main/models/')
+  faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
+  faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
+  faceapi.nets.ssdMobilenetv1.loadFromUri('/models')
 ]).then(start)
 
 async function start() {
@@ -20,7 +20,7 @@ async function start() {
   imageUpload.addEventListener('change', async () => {
     if (image) image.remove()
     if (canvas) canvas.remove()
-    //image = await faceapi.bufferToImage(imageUpload.files[0])
+    image = await faceapi.bufferToImage(imageUpload.files[0])
     //container.append(image)
     //canvas = faceapi.createCanvasFromMedia(image)
     //container.append(canvas)
@@ -31,15 +31,22 @@ async function start() {
     const results = resizedDetections.map(d => faceMatcher.findBestMatch(d.descriptor))
     const queryParams = getQueryParams(queryString);
     const key = queryParams['key'];
-    postData(results,key)
-    const dl = document.createElement('dl');
+    console.log(results)
+    const dl = document.createElement('dl'); // Create a definition list
 
     results.forEach((result, i) => {
-        dl.innerHTML += `<dt>Serial Number ${i + 1}</dt><dd>${result.toString()}</dd>`;
+        dl.innerHTML += `<dt>Serial Number ${i + 1}</dt><dd>${result.toString()}</dd>`; // Populate with result data
     });
 
-    document.body.appendChild(dl); 
+    document.body.appendChild(dl); // Append the definition list to the body or any other desired container
 
+    //postData(results,key)
+    //results.forEach((result, i) => {
+    //  console.log(result)
+    //  //const box = resizedDetections[i].detection.box
+    //  //const drawBox = new faceapi.draw.DrawBox(box, { label: result.toString() })
+    //  //drawBox.draw(canvas)
+    //})
   })
 }
 
@@ -77,23 +84,20 @@ const getQueryParams = (queryString) => {
 };
 
 
-function postData(results, key) {
-  const url = '{% url "get_result" %}';
-  const labels = results.map(result => result.label); 
-  console.log(labels);
-  $.ajax({
-    url: url,
-    type: 'POST',
-    contentType: 'application/json',
-    data: JSON.stringify({ "key": key, "labels": labels }), 
-    success: function(response) {
-      console.log('Success:', response);
+function postData(data,key) {
+  const url = 'http://127.0.0.1:8000/staffs/get_result';
+  return fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      
     },
-    error: function(error) {
-      console.error('Error:', error);
-    }
+    body: JSON.stringify({"data":data,"key":key})
+  })
+  .then(response => response.json()) 
+  .catch(error => {
+    console.error('Error:', error);
   });
 }
-
 
 
