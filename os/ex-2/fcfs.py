729fe034c2e7c75417469e9570ba3919e3841c77
Senthil Kumaran <132420106+Senthilsk10@users.noreply.github.com>
2024-05-08 07:17:16 +0530

    Delete os directory

--- os/ex-2/fcfs.py
+++
@@ -1,9 +0,0 @@
-disk_queue=list(map(int,input("disk queue:").split()))
-s=int(input("HEADER POINTER:"))
-t=0
-for i in range(len(disk_queue)):
-    if i==0:
-        t=abs(s-disk_queue[i])
-    else:
-        t+=abs(disk_queue[i-1]-disk_queue[i])
-print(f"Total Head Movements:{t}")
--- os/ex-2/scan.py
+++
@@ -1,7 +0,0 @@
-dq=list(map(int,input("disk queue:").split()))
-s=int(input("HEADER POINTER:"))
-t=0
-dq.sort()
-t+=s
-t+=max(dq)
-print(f"Total Head movements:{t}")
